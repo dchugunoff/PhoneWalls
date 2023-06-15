f562cb3522941aaf11b2d66b4e1a4ca278473972
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-06-16 02:21:57 +0300

    created SettingsFragment.kt set up bottom menu navigation

--- app/src/main/java/com/chugunov/phonewalls/presentation/MainActivity.kt
+++ app/src/main/java/com/chugunov/phonewalls/presentation/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.chugunov.phonewalls.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.chugunov.phonewalls.R
+import com.chugunov.phonewalls.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bottomNavView = binding.bottomNavView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.categorySelectionFragment -> {
+                    bottomNavView.menu.findItem(R.id.menu_item_home).isChecked = true
+                }
+                R.id.imagesFragment -> {
+                    bottomNavView.menu.findItem(R.id.menu_item_home).isChecked = true
+                }
+                else -> {
+                    bottomNavView.menu.findItem(destination.id)?.isChecked = true
+                }
+            }
+        }
+
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_item_settings -> {
+                    navController.navigate(R.id.settingsFragment2)
+                    true
+                }
+                R.id.menu_item_home -> {
+                    navController.popBackStack()
+                    navController.navigate(R.id.categorySelectionFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+    private fun getCurrentFragment(): Fragment? {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        return navHostFragment.childFragmentManager.fragments.firstOrNull()
     }
 }
--- app/src/main/java/com/chugunov/phonewalls/presentation/SelectedImageFragment.kt
+++ app/src/main/java/com/chugunov/phonewalls/presentation/SelectedImageFragment.kt
@@ -8,13 +8,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.chugunov.phonewalls.R
 import com.chugunov.phonewalls.data.downloader.ImageDownloader
 import com.chugunov.phonewalls.databinding.FragmentSelectedImageBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SelectedImageFragment : Fragment() {
 
@@ -30,6 +33,7 @@ class SelectedImageFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        (activity as AppCompatActivity).findViewById<BottomNavigationView>(R.id.bottom_nav_view)?.visibility = View.GONE
         _binding = FragmentSelectedImageBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -37,8 +41,6 @@ class SelectedImageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val imageUrl = args.imageArgs.urls.full
-        Log.d("Response", imageUrl)
-        Log.d("Response", imageUrl)
         binding.selectedImage.load(imageUrl) {
             crossfade(true)
         }
@@ -74,6 +76,7 @@ class SelectedImageFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        (activity as AppCompatActivity).findViewById<BottomNavigationView>(R.id.bottom_nav_view)?.visibility = View.VISIBLE
     }
 
     override fun onRequestPermissionsResult(
