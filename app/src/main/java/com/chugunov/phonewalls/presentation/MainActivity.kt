0162028b8a265892875405a49204806af0e8dc55
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-06-16 20:59:42 +0300

    created setting change language

--- app/src/main/java/com/chugunov/phonewalls/presentation/MainActivity.kt
+++ app/src/main/java/com/chugunov/phonewalls/presentation/MainActivity.kt
@@ -1,25 +1,38 @@
 package com.chugunov.phonewalls.presentation
 
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.preference.PreferenceManager
 import com.chugunov.phonewalls.R
 import com.chugunov.phonewalls.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var bottomNavView: BottomNavigationView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val language = sharedPreferences.getString("language", "")
+        setLocale(language)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         bottomNavView = binding.bottomNavView
+
+        Log.d("Prefer", "${sharedPreferences.getString("language", "")}")
+        Log.d("Prefer", "${sharedPreferences.contains("language")}")
+        Log.d("Prefer", "${language}")
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
@@ -52,10 +65,28 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
     }
-    private fun getCurrentFragment(): Fragment? {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        return navHostFragment.childFragmentManager.fragments.firstOrNull()
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val language = sharedPreferences.getString(getString(R.string.language_key), "")
+        setLocale(language)
+        recreate()
+    }
+
+    private fun setLocale(language: String?) {
+        if (!language.isNullOrEmpty()) {
+            val locale = Locale(language)
+            Locale.setDefault(locale)
+
+            val resources = resources
+            val configuration = resources.configuration
+            configuration.setLocale(locale)
+
+            createConfigurationContext(configuration)
+            resources.updateConfiguration(configuration, resources.displayMetrics)
+            Log.d("Prefer", "1")
+        }
     }
 }
--- app/src/main/java/com/chugunov/phonewalls/presentation/SettingsFragment.kt
+++ app/src/main/java/com/chugunov/phonewalls/presentation/SettingsFragment.kt
@@ -1,30 +1,76 @@
 package com.chugunov.phonewalls.presentation
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.chugunov.phonewalls.databinding.FragmentSettingsBinding
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import com.chugunov.phonewalls.R
+import java.util.*
 
-class SettingsFragment: Fragment() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private var _binding: FragmentSettingsBinding? = null
-    private val binding: FragmentSettingsBinding
-        get() = _binding ?: throw RuntimeException("FragmentSettingsBinding == null")
+    private lateinit var languagePreference: ListPreference
+    private lateinit var sharedPreferences: SharedPreferences
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        languagePreference = findPreference(getString(R.string.language_key))!!
+        languagePreference.summary = getLanguageSummary(languagePreference.value)
+
+        languagePreference.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
+            setLocale(newValue as String)
+            languagePreference.summary = getLanguageSummary(newValue)
+            true
+        }
     }
 
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        if (key == getString(R.string.language_key)) {
+            val language = sharedPreferences.getString(key, "")
+            setLocale(language)
+            languagePreference.summary = getLanguageSummary(language)
+        }
+    }
+
+    private fun setLocale(language: String?) {
+        if (!language.isNullOrEmpty()) {
+            val locale = Locale(language)
+            Locale.setDefault(locale)
+
+            val configuration = resources.configuration
+            configuration.setLocale(locale)
+
+            val editor = PreferenceManager.getDefaultSharedPreferences(requireContext()).edit()
+            editor.putString(getString(R.string.language_key), language)
+            editor.apply()
+
+            val context = requireContext().createConfigurationContext(configuration)
+            requireActivity().apply {
+                baseContext.resources.updateConfiguration(configuration, baseContext.resources.displayMetrics)
+                recreate()
+            }
+        }
+    }
+
+    private fun getLanguageSummary(language: String?): CharSequence? {
+        return when (language) {
+            "en" -> getString(R.string.language_english)
+            "ru" -> getString(R.string.language_russian)
+            else -> null
+        }
     }
 }
